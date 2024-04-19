77abd75809f54d198c4201cc91a5cf656533e26e
binleiden <daniel.petersson83@gmail.com>
2024-04-19 19:25:15 +0200

    Test to encrypt text in terminal. Added todo

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,6 +2,10 @@ package se.lexicon;
 
 import se.lexicon.Model.AESAlgorithm;
 import se.lexicon.Model.DESAlgorithm;
+import se.lexicon.Model.FileEncryptor;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Hello world!
@@ -10,7 +14,46 @@ import se.lexicon.Model.DESAlgorithm;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {  /*
+    Encryptfile
+    //Todo: Create method
+    try {
+        // Läs filen som en byte array
+        byte[] fileData = Files.readAllBytes(Paths.get("/home/daniel/Documents/Js odin.odt"));
+
+        // Skapa en ny instans av DESAlgorithm
+        DESAlgorithm desAlgorithm = new DESAlgorithm();
+        byte[] key = "12345678".getBytes(); // DES requires an 8 byte key
+
+        // Kryptera fildata
+        byte[] encryptedData = desAlgorithm.encrypt(fileData, key);
+
+        // Skriv den krypterade datan till en ny fil
+        Files.write(Paths.get("/home/daniel/Documents/encrypted.odt"), encryptedData);
+    } catch (Exception e) {
+        System.err.println("Error: " + e.getMessage());
+    }
+    */
+        //Todo: Create method
+        //decryptFile
+        try {
+            // Read the encrypted file as a byte array
+            byte[] encryptedData = Files.readAllBytes(Paths.get("/home/daniel/Documents/encrypted.odt"));
+
+            // Create a new instance of DESAlgorithm
+            DESAlgorithm desAlgorithm = new DESAlgorithm();
+            byte[] key = "12345678".getBytes(); // DES requires an 8 byte key
+
+            // Decrypt the file data
+            byte[] decryptedData = desAlgorithm.decrypt(encryptedData, key);
+
+            // Write the decrypted data to a new file
+            Files.write(Paths.get("/home/daniel/Documents/decrypted.odt"), decryptedData);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+
         //DES encryption
         DESAlgorithm desAlgorithm = new DESAlgorithm();
         String desOriginalText = "Hello, World!";
@@ -37,3 +80,4 @@ public class App
         System.out.println("Decrypted text: " + new String(decryptedAES));
     }
 }
+
